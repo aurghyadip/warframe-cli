14064eecac907c3dff8a8e9681179d93757e87b7
Aurghyadip <adkundu@gmail.com>
2018-08-16 10:20:56 +0530

    Added CLI support without calling node xxxx

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+#!/usr/bin/env node
+
 const program = require('commander');
 const inquirer = require('inquirer');
 const chalk = require('chalk');
 const log = console.log
 const Items = require('warframe-items');
 const columns = require('cli-columns');
-const wrap = require('wordwrap')(70);
+const wrap = require('wordwrap')(process.stdout.columns);
 const Table = require('cli-table');
 
 
@@ -19,19 +21,31 @@ function printSeparator() {
   log(s);
 }
 
+program.version('0.0.1');
+
 program.command('mod <mod_name>')
+  .description('Displays the information about a Mod.')
   .action((mod_name) => {
     mods.forEach((mod) => {
       if(mod.name === mod_name) {
         log(chalk.blue(mod.name.toUpperCase()));
         log('Description : ' + mod.description);
-        log('Polarity : ' + mod.polarity)
+        const table = new Table();
+        table.push(
+          {'Polarity' : mod.polarity},
+          {'Rarity' : mod.rarity},
+          {'Base Drain' : mod.baseDrain},
+          {'Max Rank' : mod.fusionLimit},
+          {'Max Drain' : mod.baseDrain + mod.fusionLimit},
+          {'Type' : mod.type}
+        );
+        log(table.toString())
         if (mod.hasOwnProperty('drops')) {
           log(chalk.red('DROPS FROM :'));
           drops = [];
           mod.drops.forEach((drop) => {
             chance = drop.chance * 100;
-            drops.push(drop.location + ' - ' + drop.rarity + '(' + chance.toString() + '%)');
+            drops.push(drop.location + ' - ' + drop.rarity + '(' + chance.toFixed(2).toString() + '%)');
           })
           log(columns(drops));
         }
@@ -40,30 +54,28 @@ program.command('mod <mod_name>')
   });
 
 program.command('warframe <warframe_name>')
+  .description('Displays the information about a warframe.')
   .option('--abilities', 'Lists the abilities of the warframe')
-  .option('--attributes', 'Displays the stats of the warframe')
   .option('--drops', 'Displays the parts drop locations of the warframe')
   .action((warframe_name, cmd) => {
     warframes.forEach((warframe) => {
       if(warframe.name.toLowerCase() === warframe_name.toLowerCase()) {
-        if(cmd.attributes) {
-          log(chalk.red(warframe.name.toUpperCase()));
-          log(chalk.green(wrap(warframe.description)));
-          log(chalk.yellow("\nPolarities : " + warframe.polarities.toString().toUpperCase()))
-          log(chalk.yellow("Aura Polarity : " + warframe.aura.toUpperCase()))
-          const table = new Table({
-            head: ['Attribute', 'Value']
-            , colWidths: [20, 20]
-          });
-          table.push(
-            ['Health', warframe.health],
-            ['Shield', warframe.shield],
-            ['Armor', warframe.armor],
-            ["Energy", warframe.power],
-            ["Sprint Speed", warframe.sprint]
-          );
-          log(table.toString());
-        }
+        log(chalk.red(warframe.name.toUpperCase()));
+        log(chalk.green(wrap(warframe.description)));
+        log(chalk.yellow("\nPolarities : " + warframe.polarities.toString().toUpperCase()))
+        log(chalk.yellow("Aura Polarity : " + warframe.aura.toUpperCase()))
+        const table = new Table({
+          head: ['Attribute', 'Value']
+          , colWidths: [20, 20]
+        });
+        table.push(
+          ['Health', warframe.health],
+          ['Shield', warframe.shield],
+          ['Armor', warframe.armor],
+          ["Energy", warframe.power],
+          ["Sprint Speed", warframe.sprint]
+        );
+        log(table.toString());
 
         if (cmd.abilities) {
           warframe.abilities.forEach((ability) => {
@@ -82,7 +94,7 @@ program.command('warframe <warframe_name>')
                 drops = [];
                 component.drops.forEach((drop) => {
                   chance = drop.chance * 100
-                  drops.push(drop.location + '(' + chance.toString() + '%)');
+                  drops.push(drop.location + '(' + chance.toFixed(2).toString() + '%)');
                 })
                 log(columns(drops));
                 printSeparator();
